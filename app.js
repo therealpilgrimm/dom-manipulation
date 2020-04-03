35ec1f96c0fe1799538fd8c17fe4a0f655def749
therealpilgrimm <patrickbrinca@gmail.com>
2020-04-03 19:02:42 +0200

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
@@ -23,23 +23,42 @@ document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 
-function btn() {
+/* function btn() {
    //do something here
-}
+} */
 
-document.querySelector('.btn-roll').addEventListener('click', function btn() {
+document.querySelector('.btn-roll').addEventListener('click', function btn() {      //<-- Anonymous function
   
     // 1. Random number
     var dice = Math.floor(Math.random()*6) + 1;
 
-    // Display the result
+    // 2. Display the result
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
     diceDOM.src = 'dice-' + dice + '.png';
-    
 
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; 
+        roundScore = 0;
 
-    // Update the round score IF the rolled number was NOT a 1
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+
+    }
 
 });
 
